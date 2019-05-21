d572974eb872ade8e31e445d1fabfc269a27fd29
kasper <kasper1985@gmail.com>
2019-05-21 22:48:17 +0200

    Added components:  - header  - home  - nav-panel  - footer Divided page into areas for a future web page Shrink/grow animation for a navigation panel Added fonts to application New favicon Added translation to the application Defined color schema with an empty content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,48 @@
+// Angular modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+// NGX Modules
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+// Application
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavPanelComponent } from './nav-panel/nav-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavPanelComponent
   ],
   imports: [
+    // Angular modules
     BrowserModule,
+
+    // NGX modules
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient]
+      }
+    }),
+
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/languages/', '.json');
+}
